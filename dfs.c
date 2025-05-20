afcdaf7907cccee2fdc3cd577b7992492ac276e2
LelouchelawlietLIEBERT <154049616+LelouchelawlietLIEBERT@users.noreply.github.com>
2025-05-20 07:09:51 +0530

    Update dfs.c

--- dfs.c
+++ dfs.c
@@ -1,36 +1,31 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <limits.h> // For INT_MAX
+#include <stdlib.h>
 
-#define MAX_NODES 5
-
-// Adjacency matrix representing the directed weighted graph
-int graph[MAX_NODES][MAX_NODES] = {
-    {0, 1, 1, 1, 0},
-    {1, 0, 1, 0, 1},
-    {1, 1, 0, 2, 1},
-    {1, 0, 2, 0, 1},
-    {0, 1, 1, 1, 0},
-};
-
-void DFS(int node, bool visited[]) {
-    visited[node] = true;
-    printf("%d ", node);
-
-    for (int i = 0; i < MAX_NODES; i++) {
-        if (graph[node][i] != 0 && !visited[i]) {
-            DFS(i, visited);
-        }
-    }
-}
+int *adj[100], deg[100], vis[100], q[100]; // adjacency list, degree, visited, queue
 
 int main() {
-    int source = 0; // Source node (starting node)
-    bool visited[MAX_NODES] = {false}; // Initialize all nodes as not visited
+    int V, E, i, u, v, src, f = 0, r = 0;
 
-    printf("Nodes reachable from node %d are: ", source);
-    DFS(source, visited);
-    printf("\n");
+    scanf("%d%d", &V, &E); // input number of vertices and edges
+    while (E--) {
+        scanf("%d%d", &u, &v);
+        // allocate space for each new adjacent node (4 bytes = sizeof(int))
+        adj[u] = realloc(adj[u], ++deg[u]*4);
+        adj[u][deg[u]-1] = v;
+    }
 
-    return 0;
+    scanf("%d", &src);     // input source node
+    q[r++] = src;          // enqueue source
+    vis[src] = 1;
+
+    while (f < r) {        // BFS loop
+        u = q[f++];        // dequeue
+        printf("%d ", u);  // print current node
+        for (i = 0; i < deg[u]; i++) {
+            if (!vis[adj[u][i]]) {
+                q[r++] = adj[u][i];      // enqueue neighbor
+                vis[adj[u][i]] = 1;      // mark as visited
+            }
+        }
+    }
 }

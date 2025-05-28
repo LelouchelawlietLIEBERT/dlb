367c4ca8196f1634bac411e9880e8c37ede31970
Ashwin Gowda <154049616+LelouchelawlietLIEBERT@users.noreply.github.com>
2025-05-28 21:58:48 +0530

    Update b4.py

--- b4.py
+++ b4.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("TATASTEEL.csv")
 df['Date'] = pd.to_datetime(df['Date'])
-
 plt.figure(figsize=(12,6))
 plt.plot(df['Date'], df['Close'], label='Close')
 plt.scatter(df['Date'][::10], df['Close'][::10], color='r', s=10)
@@ -11,7 +10,4 @@ plt.xlabel("Date")
 plt.ylabel("Price")
 plt.title("TATASTEEL Closing Prices Over Time")
 plt.grid(True)
-plt.legend()
-plt.xticks(rotation=45)
-plt.tight_layout()
 plt.show()

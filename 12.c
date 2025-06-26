d52b017f3a2ca5657d3540e10e95e4cf73706fcd
Ashwin Gowda <154049616+LelouchelawlietLIEBERT@users.noreply.github.com>
2025-06-27 00:00:58 +0530

    Update 12.c

--- 12.c
+++ 12.c
@@ -1,39 +1,62 @@
 #include <stdio.h>
 #include <limits.h>
 
-int g[5][5] = {
+#define N 5
+
+int g[N][N] = {
     {0, 2, INT_MAX, 12, 5},
     {2, 0, 4, 8, INT_MAX},
     {INT_MAX, 4, 0, 3, 3},
     {12, 8, 3, 0, 10},
-    {5, INT_MAX, 3, 10, 0}
-};
+    {5, INT_MAX, 3, 10, 0}};
 
-int p[6], v[5], min = INT_MAX, res[6];
+int p[N + 1], v[N] = {0}, min_cost = INT_MAX, res[N + 1];
 
-void tsp(int l, int cost) {
-    if (l == 5) {
-        int total = cost + g[p[4]][p[0]];
-        if (total < min) {
-            min = total;
-            for (int i = 0; i < 5; i++) res[i] = p[i];
-            res[5] = p[0];
+void tsp(int l, int cost)
+{
+    if (l == N)
+    {
+        if (g[p[N - 1]][p[0]] != INT_MAX)
+        {
+            int total = cost + g[p[N - 1]][p[0]];
+            if (total < min_cost)
+            {
+                min_cost = total;
+                for (int i = 0; i < N; i++)
+                    res[i] = p[i];
+                res[N] = p[0];
+            }
         }
         return;
     }
-    for (int i = 0; i < 5; i++) {
-        if (!v[i] && g[p[l-1]][i] != INT_MAX) {
+
+    for (int i = 0; i < N; i++)
+    {
+        if (!v[i] && g[p[l - 1]][i] != INT_MAX)
+        {
             v[i] = 1;
             p[l] = i;
-            tsp(l+1, cost + g[p[l-1]][i]);
+            tsp(l + 1, cost + g[p[l - 1]][i]);
             v[i] = 0;
         }
     }
 }
 
-int main() {
-    p[0] = 0; v[0] = 1;
+int main()
+{
+    p[0] = 0;
+    v[0] = 1;
     tsp(1, 0);
-    printf("Cost: %d\nPath: ", min);
-    for (int i = 0; i <= 5; i++) printf("%c ", 'A' + res[i]);
+
+    if (min_cost == INT_MAX)
+        printf("No valid tour found.\n");
+    else
+    {
+        printf("Minimum Cost: %d\nPath: ", min_cost);
+        for (int i = 0; i <= N; i++)
+            printf("%c ", 'A' + res[i]);
+        printf("\n");
+    }
+
+    return 0;
 }

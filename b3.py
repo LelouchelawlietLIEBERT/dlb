a9ef2384a1a3b95ce93062e3ef0d42f47a92c496
Ashwin Gowda <154049616+LelouchelawlietLIEBERT@users.noreply.github.com>
2025-07-01 23:17:30 +0530

    Update b3.py

--- b3.py
+++ b3.py
@@ -1,9 +1,8 @@
 from openpyxl import load_workbook
-wb = load_workbook("sales.xlsx"); ws = wb.active
-data = list(ws.iter_rows(values_only=True))[1:]
-sales = [(r[0], r[1]*r[2]) for r in data]
-print("Total Sales:", sum(x[1] for x in sales))
-print("Total Qty:", sum(r[1] for r in data))
-print("Avg Price:", sum(r[2] for r in data)/len(data))
-print("Best:", max(sales, key=lambda x: x[1])[0])
-print("Expensive:", max(data, key=lambda x: x[2])[0])
+w = load_workbook("sales.xlsx").active
+data = list(w.iter_rows(values_only=True))[1:]
+print("Total Sales :",sum(i[2]*i[1] for i in data))
+print("Total Quantity :",sum(i[1] for i in data))
+print("Avg Price per product :",sum(i[2]*i[1] for i in data)/sum(i[1] for i in data))
+print("Most Expensive Product :",sorted(data,key=lambda x:x[2])[-1][0])
+print("Best Selling Product :",sorted(data,key=lambda x:x[1])[-1][0])
